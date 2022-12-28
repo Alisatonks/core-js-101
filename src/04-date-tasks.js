d433387c4d6c9aa7469f1708f2f213dc724e33c9
Volha Haiduk <Alisatonks2385@gmail.com>
2022-12-28 01:19:17 +0100

    feat: add solution for dates

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -19,8 +19,8 @@
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromRfc2822(value) {
+  return Date.parse(value);
 }
 
 /**
@@ -34,8 +34,8 @@ function parseDataFromRfc2822(/* value */) {
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601(value) {
+  return Date.parse(value);
 }
 
 
@@ -53,8 +53,12 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const year = date.getFullYear();
+  const myDate = new Date(year, 1, 29);
+  if (myDate.getDate() === 29) {
+    return true;
+  } return false;
 }
 
 
@@ -73,8 +77,16 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  const deltaHours = endDate.getHours() - startDate.getHours();
+  const deltaHoursRes = deltaHours < 9 ? `0${deltaHours}` : deltaHours;
+  const deltaMinutes = endDate.getMinutes() - startDate.getMinutes();
+  const deltaMinRes = deltaMinutes < 9 ? `0${deltaMinutes}` : deltaMinutes;
+  const deltaSec = endDate.getSeconds() - startDate.getSeconds();
+  const deltaSecRes = deltaSec < 9 ? `0${deltaSec}` : deltaSec;
+  const deltaMilliSec = endDate.getMilliseconds() - startDate.getMilliseconds();
+  const deltaMilliSecRes = deltaMilliSec > 100 ? `${deltaMilliSec}` : `00${deltaMilliSec}`;
+  return `${deltaHoursRes}:${deltaMinRes}:${deltaSecRes}.${deltaMilliSecRes}`;
 }
 
 
